221f5fe1ad1b1aef8114b821ee9f825b5187a1db
Abby Sova <43381584+abbysova@users.noreply.github.com>
2019-03-28 16:58:26 -0500

    Initial commit

---
+++ app/src/main/java/com/example/participation_10/DaoWord.java
@@ -0,0 +1,14 @@
+package com.example.participation_10;
+
+public interface DaoWord {
+
+    @Insert
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAllWords();
+}
+
