4ad1244fd9238d570031436e63edcf60e376f2a0
Abby Sova <43381584+abbysova@users.noreply.github.com>
2019-03-28 22:08:07 -0500

    fix errors

--- app/src/main/java/com/example/participation_10/Word.java
+++ app/src/main/java/com/example/participation_10/Word.java
@@ -1,27 +1,24 @@
 package com.example.participation_10;
 
 import android.arch.persistence.room.ColumnInfo;
-
 import android.arch.persistence.room.Entity;
-
 import android.arch.persistence.room.PrimaryKey;
-
 import android.support.annotation.NonNull;
 
+@Entity(tableName = "word_table")
+public class Word {
 
-    public class Word {
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
 
-        @Entity(tableName = "word_table")
-        public class Word {
+    public Word(@NonNull String word) {
+        this.mWord = word;
+    }
 
-            @PrimaryKey
-            @NonNull
-            @ColumnInfo(name = "word")
-            private String mWord;
-
-            public Word(String word) {this.mWord = word;}
-
-            public String getWord(){return this.mWord;}
-        }
+    @NonNull
+    public String getWord() {
+        return this.mWord;
     }
 }
