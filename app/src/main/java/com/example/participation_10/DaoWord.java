b55512c19975845a2bd8a96bb68436b23355c75b
Abby Sova <43381584+abbysova@users.noreply.github.com>
2019-03-28 22:10:30 -0500

    fix imports add calls

--- app/src/main/java/com/example/participation_10/DaoWord.java
+++ app/src/main/java/com/example/participation_10/DaoWord.java
@@ -1,14 +1,19 @@
 package com.example.participation_10;
 
-public interface DaoWord {
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import java.util.List;
+@Dao
+public interface WordDao {
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAlphabetizedWords();
 
     @Insert
     void insert(Word word);
 
     @Query("DELETE FROM word_table")
     void deleteAll();
-
-    @Query("SELECT * from word_table ORDER BY word ASC")
-    LiveData<List<Word>> getAllWords();
 }
 
