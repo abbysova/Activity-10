48c83cf5a99d1799f6a80ec12374583c5096aa0a
Abby Sova <43381584+abbysova@users.noreply.github.com>
2019-03-28 22:06:59 -0500

    Corrected imports

--- app/src/main/java/com/example/participation_10/NewWordActivity.java
+++ app/src/main/java/com/example/participation_10/NewWordActivity.java
@@ -1,4 +1,15 @@
+package com.example.participation_10;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 public class NewWordActivity extends AppCompatActivity {
+
 
     public static final String EXTRA_REPLY = "com.example.android.wordlistsql.REPLY";
 
